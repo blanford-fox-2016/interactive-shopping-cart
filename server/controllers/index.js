de0cb53958ea115a65ef61f878ff11727a084d89
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-01 16:33:47 +0700

    update kelar

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -44,5 +44,43 @@ module.exports = {
                 customer: customer
             })
         })
+    },
+
+    getCustomerEdit: function (req, res) {
+        Customer.findOne({
+            _id: req.params.id
+        }, function (err, customer) {
+            if (err) {
+                throw err
+            }
+
+            res.json({
+                status: "get customer",
+                customer: customer
+            })
+        })
+    },
+
+    updateCustomer: function (req, res) {
+        Customer.findByIdAndUpdate(
+            req.params.id, {
+                name: req.body.name,
+                memberId: req.body.memberId,
+                address: req.body.address,
+                zip: req.body.zip,
+                phone: req.body.phone
+            },
+            function (err) {
+                console.log('updated!')
+            }
+        )
+        Customer.findById(req.params.id, function (err, data) {
+            if (err) throw err
+            console.log('found it!!!!!!!!')
+            res.json({
+                status: "Customer di update",
+                customer: data
+            })
+        })
     }
 }
--- server/routes/api.js
+++ server/routes/api.js
@@ -3,9 +3,9 @@ var router = express.Router();
 var controller = require('../controllers/index')
 
 router.get('/customer', controller.getCustomer)
-
 router.post('/customer/create', controller.createCustomer)
-
 router.delete('/customer/delete/:id', controller.deleteCustomer)
+router.get('/customer/update/:id', controller.getCustomerEdit)
+router.put('/customer/update/:id', controller.updateCustomer)
 
 module.exports = router;
