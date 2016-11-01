3ee73cdcfe9c9de65cb2092e80801228f43fc31b
Alexander Hendrawan tjahja <email": null>
2016-11-01 13:50:00 +0700

    update ke 2

--- server/app.js
+++ server/app.js
@@ -5,9 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var customer = require('./routes/customer')
 var api = require('./routes/api')
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/shopping-cart');
@@ -26,9 +23,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/customer', customer)
-app.use('/users', users);
 app.use('/api', api)
 
 // catch 404 and forward to error handler
--- server/routes/customer.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var controller = require('../controllers/customerController')
-
-
-/* GET home page. */
-router.get('/', controller.customerList);
-router.post('/', controller.customerCreate)
-module.exports = router;
