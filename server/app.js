1f436742b764f8af841ff81effde37607595a9bd
Septian A Tama <sfirst.tama@gmail.com>
2016-11-01 10:58:21 +0700

    initiate cors

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const routes = require('./routes/index');
 const users = require('./routes/users');
@@ -30,6 +31,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', routes);
 app.use('/users', users);
