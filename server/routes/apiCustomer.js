b717af32366bc4a2d22432b8b7fc205ea5fba6d2
Septian A Tama <sfirst.tama@gmail.com>
2016-11-01 13:41:43 +0700

    simplifying routes of api customer

--- server/routes/apiCustomer.js
+++ server/routes/apiCustomer.js
@@ -4,5 +4,9 @@ const customer = require('../controllers/customerController')
 
 /* GET users listing. */
 router.get('/', customer.all);
+router.get('/:id', customer.one);
+router.post('/', customer.add);
+router.put('/:id', customer.edit);
+router.delete('/:id', customer.destroy);
 
 module.exports = router;
