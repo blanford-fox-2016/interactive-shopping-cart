97294aa5c2ff3f82180bba275d39f02b026377c9
Ken Duigraha utra <kenduigraha@yahoo.com>
2016-11-04 03:33:51 +0700

    updated carts.html & cart.js (still in process)

--- server/controllers/api.carts.js
+++ server/controllers/api.carts.js
@@ -14,6 +14,7 @@ let allCarts = (req, res, next) => {
 }
 
 let addCart = (req, res, next) => {
+  console.log(req.body.itemList);
   Carts.create({
     memberId          : req.body.memberId,
     total             : req.body.total,
