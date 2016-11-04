1e299cb08ab060ce8bef706a731808bac338777f
Ahyana Rizky Pratama <ahyana.rizky@gmail.com>
2016-11-04 20:10:28 +0700

    fix UI

--- client/src/javascripts/javascript.js
+++ client/src/javascripts/javascript.js
@@ -6,8 +6,8 @@ function loadTableItem() {
             var data = JSON.parse(xhr.responseText)
             html = '<h2>Item List</h2>'
             for (var i = 0; i < data.length; i++) {
-                html += `<div class="panel panel-info"><div class="panel-heading"><h3> ${data[i].itemCode} - ${data[i].name}</h3><div class="clearfix"></div></div><div class="panel-body"><p id=tweets> ${data[i].description}</p><div class='well'>Price : ${data[i].price}
-              Stock : ${data[i].stock}</div></div></div>`
+                html += `<div class="panel panel-success"><div class="panel-heading"><h3> ${data[i].itemCode} - ${data[i].name}</h3><div class="clearfix"></div></div><div class="panel-body"> <div class='well well-sm'><strong>${data[i].description}</strong></div><div class='well well-sm'>Price : ${data[i].price}</div>
+              <div class='well well-sm'>Stock : ${data[i].stock}</div></div></div>`
             }
             document.getElementById('itempanel').className = "active"
             document.getElementById('customerpanel').className = ""
@@ -29,8 +29,8 @@ function loadTableCustomer() {
             var data = JSON.parse(xhr.responseText)
             html = '<h2>Customer List</h2>'
             for (var i = 0; i < data.length; i++) {
-                html += `<div class="panel panel-info"><div class="panel-heading"><h3> ${data[i].memberId} - ${data[i].name}</h3><div class="clearfix"></div></div><div class="panel-body"><div class='well'> Address : ${data[i].address}</div><div class='well'>ZIP : ${data[i].zip}</div>
-              <div class='well'>Phone : ${data[i].phone}</div></div></div>`
+                html += `<div class="panel panel-primary"><div class="panel-heading"><h3> ${data[i].memberId} - ${data[i].name}</h3><div class="clearfix"></div></div><div class="panel-body"><div class='well well-sm'> Address : ${data[i].address}</div><div class='well well-sm'>ZIP : ${data[i].zip}</div>
+              <div class='well well-sm'>Phone : ${data[i].phone}</div></div></div>`
             }
             document.getElementById('itempanel').className = ""
             document.getElementById('customerpanel').className = "active"
