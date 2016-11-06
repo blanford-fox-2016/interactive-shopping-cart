6fc938d1451f19ac53e229fadf7ee26b8d24113b
Ahyana Rizky Pratama <ahyana.rizky@gmail.com>
2016-11-06 15:32:44 +0700

    item list progrss

--- client/src/javascripts/javascript.js
+++ client/src/javascripts/javascript.js
@@ -4,20 +4,67 @@ function loadTableItem() {
     xhr.onload = function() {
         if (xhr.status === 200) {
             var data = JSON.parse(xhr.responseText)
-            html = '<h2>Item List</h2>'
+            html = `<table class='table table-hover'>
+            <thead>
+            <tr>
+            <td>Item Code
+            </td>
+            <td>Name
+            </td>
+            <td>Description
+            </td>
+            <td>Price
+            </td>
+            <td>Stock
+            </td>
+            <td>Action
+            </td>
+            </tr>
+            </thead>
+            <tbody>`
             for (var i = 0; i < data.length; i++) {
-                html += `<div class="panel panel-success"><div class="panel-heading"><h3> ${data[i].itemCode} - ${data[i].name}</h3><div class="clearfix"></div></div><div class="panel-body"> <div class='well well-sm'><strong>${data[i].description}</strong></div><div class='well well-sm'>Price : ${data[i].price}</div>
-              <div class='well well-sm'>Stock : ${data[i].stock}</div>
-              <button id='upd${data[i]._id}' data-id='${data[i]._id}' class='btn btn-warning' role='button' onclick='return formEditItem(this)'>Update</button>
-              <button id='del${data[i]._id}' data-id='${data[i]._id}' class='btn btn-danger' role='button' onclick='return formEditItem(this)'>Delete</button>
-              <div id='${data[i]._id}'  hidden>
-              </div></div></div>`
+                html += `<tr id="rowItem${data[i]._id}"><td>${data[i].itemCode}</td>
+                        <td>${data[i].name}</td>
+                        <td>${data[i].description}</td>
+                        <td>${data[i].price}</td>
+                        <td>${data[i].stock}</td>
+                        <td><span>
+                            <button id="buttonEditItem${data[i]._id}" class="btn btn-warning" onclick="formEditItem('${data[i]._id}')">Edit</button>
+                            <button id="buttonDeleteItem${data[i]._id}" class="btn btn-danger" onclick="deleteItem('${data[i]._id}')">Delete</button>
+                        </span></td></tr>`
             }
+            html += `</tbody></table>
+            <div class="page-header">
+              <h2>Add or Manage Item</h2>
+            </div><div class='container'>
+            <form id='itemForm'>
+  <div class="form-group">
+    <label for="itemCode_new">Item Code</label>
+    <input type="text" class="form-control" id="input_itemCode" placeholder="Item Code" name='itemCode'>
+  </div>
+  <div class="form-group">
+  <label for="name_new">Item Name</label>
+  <input type="text" class="form-control" id="input_name" placeholder="Item Name" name='name'>
+  </div>
+  <div class="form-group">
+  <label for="description_new">Description</label>
+  <input type="text" class="form-control" id="input_description" placeholder="Description" name='description'>
+  </div>
+  <div class="form-group">
+  <label for="price_new">Price</label>
+  <input type="text" class="form-control" id="input_price" placeholder="Price" name='price'>
+  </div>
+  <div class="form-group">
+  <label for="stock_new">Stock</label>
+  <input type="text" class="form-control" id="input_stock" placeholder="Stock" name='stock'>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form></div>`
             document.getElementById('itempanel').className = "active"
             document.getElementById('customerpanel').className = ""
             document.getElementById('transactionpanel').className = ""
 
-            document.getElementById('list').innerHTML = html
+            document.getElementById('table-item').innerHTML = html
         } else {
             alert('Request failed.  Returned status of ' + xhr.status);
         }
@@ -31,19 +78,40 @@ function loadTableCustomer() {
     xhr.onload = function() {
         if (xhr.status === 200) {
             var data = JSON.parse(xhr.responseText)
-            html = '<h2>Customer List</h2>'
+            html = `<thead>
+            <tr>
+            <td>Item Code
+            </td>
+            <td>Name
+            </td>
+            <td>Description
+            </td>
+            <td>Price
+            </td>
+            <td>Stock
+            </td>
+            <td>Action
+            </td>
+            </tr>
+            </thead>
+            <tbody>`
             for (var i = 0; i < data.length; i++) {
-                html += `<div class="panel panel-primary"><div class="panel-heading"><h3> ${data[i].memberId} - ${data[i].name}</h3><div class="clearfix"></div></div><div class="panel-body"><div class='well well-sm'> Address : ${data[i].address}</div><div class='well well-sm'>ZIP : ${data[i].zip}</div>
-              <div class='well well-sm'>Phone : ${data[i].phone}</div>
-<button id='upd${data[i]._id}' data-id='${data[i]._id}' class='btn btn-warning' role='button' onclick='return formEditItem(this)'>Update</button>
-<button id='del${data[i]._id}' data-id='${data[i]._id}' class='btn btn-danger' role='button' onclick='return formEditItem(this)'>Delete</button>
-              <div id='${data[i]._id}' data-id='${data[i]._id}' hidden></div></div></div>`
+                html += `<tr id="rowItem${data[i]._id}"><td>${data[i]._itemCode}</td>
+                        <td>${data[i].name}</td>
+                        <td>${data[i].description}</td>
+                        <td>${data[i].price}</td>
+                        <td>${data[i].stock}</td>
+                        <td><span>
+                            <button id="buttonEditItem${data[i]._id}" class="btn btn-warning" onclick="formEditItem('${data[i]._id}')">Edit</button>
+                            <button id="buttonDeleteItem${data[i]._id}" class="btn btn-danger" onclick="deleteItem('${data[i]._id}')">Delete</button>
+                        </span></td></tr>`
             }
+            html += `</tbody>`
             document.getElementById('itempanel').className = ""
             document.getElementById('customerpanel').className = "active"
             document.getElementById('transactionpanel').className = ""
 
-            document.getElementById('list').innerHTML = html
+            document.getElementById('table-item').innerHTML = html
         } else {
             alert('Request failed.  Returned status of ' + xhr.status);
         }
--- server/models/cart.js
+++ server/models/cart.js
@@ -2,7 +2,17 @@
 
 const mongoose = require('mongoose');
 let connection = mongoose.createConnection(process.env.DATABASE);
-let cartSchema = new mongoose.Schema({memberId: String, total: Number, transaction_date: Date, itemList: Array});
+let cartSchema = new mongoose.Schema({
+    memberId: [
+        {
+            type: String,
+            ref: 'Customer'
+        }
+    ],
+    total: Number,
+    transaction_date: Date,
+    itemList: Array
+});
 
 let Cart = connection.model('Cart', cartSchema);
 module.exports = Cart
